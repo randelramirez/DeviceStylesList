346445489a50d0dc5bf997bd72bbf3904056154b
randelramirez <randelramirez1@gmail.com>
2016-10-22 20:06:45 +0800

    Add project files.

---
+++ DeviceStylesList/DeviceStylesList/DeviceStylesListPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DeviceStylesList
+{
+    public partial class DeviceStylesListPage : ContentPage
+    {
+        public DeviceStylesListPage()
+        {
+            InitializeComponent();
+            var styleItems = new[]
+            {
+                new { style = (Style)null, name = "No style whatsoever" },
+                new { style = Device.Styles.BodyStyle, name = "Body Style" },
+                new { style = Device.Styles.TitleStyle, name = "Title Style" },
+                new { style = Device.Styles.SubtitleStyle, name = "Subtitle Style" },
+
+                // Derived style
+                new { style = new Style(typeof(Label))
+                {
+                    BaseResourceKey = Device.Styles.SubtitleStyleKey,
+                    Setters =
+                    {
+                        new Setter
+                        {
+                            Property = Label.TextColorProperty,
+                            Value = Color.Accent
+                        },
+                        new Setter
+                        {
+                            Property = Label.FontAttributesProperty,
+                            Value = FontAttributes.Italic
+                        }
+                    }
+                }, name = "New Subtitle Style" },
+
+                new { style = Device.Styles.CaptionStyle, name = "Caption Style" },
+                new { style = Device.Styles.ListItemTextStyle, name = "List Item Text Style" },
+                new { style = Device.Styles.ListItemDetailTextStyle,
+                      name = "List Item Detail Text Style" },
+            };
+
+            foreach (var styleItem in styleItems)
+            {
+                codeLabelStack.Children.Add(new Label
+                {
+                    Text = styleItem.name,
+                    Style = styleItem.style
+                });
+            }
+        }
+    }
+}
